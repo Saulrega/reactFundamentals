5751ab816f8940d64f574a6a54f9cae03e33d98e
Saúl Regalado <saul.regalado@boldmoves.mx>
2025-01-09 12:51:56 -0600

    feat: customHooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,25 @@
-import { useEffect, useState } from 'react'
 import './App.css'
+import { useFetch } from './hooks';
+
+const url = "https://api.example/data";
+
+interface Data{
+  name: string;
+  lastName: string;
+  age: number;
+}
+
 
 function App() {
-  const [data, setData] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState("")
 
-
-  const fetchData = async () => {
-    setLoading(true)
-    try{
-      const response = await fetch("https://api.example/data");
-      if(!response.ok){
-        throw new Error("Error al obtener datos")
-      }
-      const jsonData = await response.json();
-      
-      setData(jsonData)
-    }catch(err){
-      setError(err as string)
-    }finally{
-      setLoading(true)
-    }
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
-
+  const { data, error, loading } = useFetch<Data>(url);
+  
   if (loading){
     return <div>Cargando...</div>
   }
 
   if (error){
-    return <div>Ups!! Hay un error: {error}</div>
+    return <div>Ups!! Hay un error: {error.message}</div>
   }
 
   return(
